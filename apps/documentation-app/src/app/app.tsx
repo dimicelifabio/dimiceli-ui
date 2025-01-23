b1309862e35a580dd0c73ff198e17060dd4e6658
Fabio Di Miceli <fabio25mich@gmail.com>
2025-01-23 15:17:48 +0100

    feat: "create component footer"

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -3,10 +3,14 @@ import { Button } from '@dimiceli-ui/button';
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
+import { Footer } from '@dimiceli-ui/footer';
 
 export function App() {
   return (
-    <Button/>
+    <>
+      <Button />
+      <Footer />
+    </>
   );
 }
 
