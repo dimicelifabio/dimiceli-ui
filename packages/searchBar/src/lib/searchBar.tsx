90148c7cab4e049d22dba98ba340d981698bbb63
Fabio Di Miceli <fabio25mich@gmail.com>
2025-01-23 14:08:42 +0100

    fix: "create nwe components"

---
+++ packages/searchBar/src/lib/searchBar.tsx
@@ -0,0 +1,11 @@
+import styles from './searchBar.module.css';
+
+export function SearchBar() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SearchBar!</h1>
+    </div>
+  );
+}
+
+export default SearchBar;
